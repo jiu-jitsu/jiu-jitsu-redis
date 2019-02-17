e83fa4ddb3bafa65321416d915653daa49da3b31
vitaliks <vitaly@anagorsky.com>
2019-02-17 13:24:28 +0300

    0.0.1

--- sources/index.js
+++ sources/index.js
@@ -20,9 +20,21 @@ const ___protocol = require('./protocol')
 
 class Redis extends events {
 
+	/**
+	 *
+	 */
+
 	constructor (endpoint) {
 
+		/**
+		 *
+		 */
+
 		super()
+
+		/**
+		 *
+		 */
 
 		this.___buffers = []
 		this.___callbacks = []
@@ -44,6 +56,10 @@ class Redis extends events {
 
 		if (!this.___endpoint.password) {
 
+			/**
+			 *
+			 */
+
 			return
 
 		}
@@ -56,6 +72,10 @@ class Redis extends events {
 		const buffer = this.___socket.___protocol.write(cmd)
 		const callback = (error) => {}
 
+		/**
+		 *
+		 */
+
 		this.___buffers.push(buffer)
 		this.___callbacks.push(callback)
 		this.___write()
@@ -63,6 +83,10 @@ class Redis extends events {
 	}
 
 	___connect () {
+
+		/**
+		 *
+		 */
 
 		this.___socket = new net.Socket()
 		this.___socket
@@ -77,20 +101,40 @@ class Redis extends events {
 
 	___reconnect (error) {
 
+		/**
+		 *
+		 */
+
 		const callbacks = this.___callbacks
 
+		/**
+		 *
+		 */
+
 		callbacks.forEach((callback) => callback(___error('jiu-jitsu-redis/REDIS_CONNECTION_HAS_BEEN_CLOSED', error)))
+
+		/**
+		 *
+		 */
 
 		this.___buffers = []
 		this.___callbacks = []
 		this.___reconnecting = true
 		this.___socket.___protocol.flush()
 
+		/**
+		 *
+		 */
+
 		setTimeout(() => this.___connect(), this.___timeout)
 
 	}
 
 	___protocol () {
+
+		/**
+		 *
+		 */
 
 		this.___socket.___protocol = new ___protocol()
 		this.___socket.___protocol.on('message', (message) => this.___onMessage(message))
@@ -99,11 +143,23 @@ class Redis extends events {
 
 	___onConnect (error) {
 
+		/**
+		 *
+		 */
+
 		this.___protocol()
 		this.___auth()
 		this.___reconnecting = false
 
+		/**
+		 *
+		 */
+
 		if (!this.___connected) {
+
+			/**
+			 *
+			 */
 
 			this.___connected = true
 			process.nextTick(() => this.emit('ready'))
@@ -114,11 +170,19 @@ class Redis extends events {
 
 	___onError (error) {
 
+		/**
+		 *
+		 */
+
 		this.___reconnect(error)
 
 	}
 
 	___onData (chunk) {
+
+		/**
+		 *
+		 */
 
 		this.___socket.___protocol.read(chunk)
 
@@ -126,15 +190,31 @@ class Redis extends events {
 
 	___onEnd (error) {
 
+		/**
+		 *
+		 */
+
 		this.___reconnect()
 
 	}
 
 	___onMessage (message) {
 
+		/**
+		 *
+		 */
+
 		const callback = this.___callbacks.shift()
 
+		/**
+		 *
+		 */
+
 		if (callback) {
+
+			/**
+			 *
+			 */
 
 			return callback(message.error, message.data) | this.___write()
 
@@ -144,13 +224,25 @@ class Redis extends events {
 
 	___write () {
 
+		/**
+		 *
+		 */
+
 		if (!this.___buffers.length) {
+
+			/**
+			 *
+			 */
 
 			return
 
 		}
 
 		const buffer = Buffer.concat(this.___buffers)
+
+		/**
+		 *
+		 */
 
 		this.___buffers = []
 		this.___socket.write(buffer)
@@ -159,14 +251,30 @@ class Redis extends events {
 
 	lua (script, callback) {
 
+		/**
+		 *
+		 */
+
 		if (!this.___connected || this.___reconnecting) {
+
+			/**
+			 *
+			 */
 
 			return callback(___error('jiu-jitsu-redis/REDIS_SOCKET_IS_NOT_READY'))
 
 		}
 
+		/**
+		 *
+		 */
+
 		const cmd = ['EVAL', script, 0]
 		const buffer = this.___socket.___protocol.write(cmd)
+
+		/**
+		 *
+		 */
 
 		this.___buffers.push(buffer)
 		this.___callbacks.push(callback)
--- sources/protocol.js
+++ sources/protocol.js
@@ -11,9 +11,21 @@ const events = require('events')
 
 class Protocol extends events {
 
+	/**
+	 *
+	 */
+
 	constructor () {
 
+		/**
+		 *
+		 */
+
 		super()
+
+		/**
+		 *
+		 */
 
 		this.___read = {}
 		this.___read.buffer = Buffer.alloc(0)
@@ -22,31 +34,67 @@ class Protocol extends events {
 
 	flush () {
 
+		/**
+		 *
+		 */
+
 		this.___read.buffer = Buffer.alloc(0)
 
 	}
 
 	read (chunk) {
 
+		/**
+		 *
+		 */
+
 		if (chunk) {
+
+			/**
+			 *
+			 */
 
 			this.___read.buffer = Buffer.concat([this.___read.buffer, chunk])
 
 		}
 
+		/**
+		 *
+		 */
+
 		if (!this.___read.buffer.length) {
+
+			/**
+			 *
+			 */
 
 			return
 
 		}
+
+		/**
+		 *
+		 */
 
 		const index_of_crlf = this.___read.buffer.indexOf(13)
 
+		/**
+		 *
+		 */
+
 		if (index_of_crlf < 0) {
+
+			/**
+			 *
+			 */
 
 			return
 
 		}
+
+		/**
+		 *
+		 */
 
 		const message = {
 			id: null,
@@ -64,12 +112,28 @@ class Protocol extends events {
 
 		if (this.___read.buffer[0] === 43) {
 
+			/**
+			 *
+			 */
+
 			message.ok = true
 			message.data = this.___read.buffer.slice(1, index_of_crlf).toString()
 
+			/**
+			 *
+			 */
+
 			this.___read.buffer = this.___read.buffer.slice(index_of_crlf + 2, this.___read.buffer.length)
 
+			/**
+			 *
+			 */
+
 		} else if (this.___read.buffer[0] === 36) {
+
+			/**
+			 *
+			 */
 
 			const size_as_string = this.___read.buffer.slice(1, index_of_crlf).toString()
 
@@ -79,50 +143,122 @@ class Protocol extends events {
 
 			if (size_as_string === '-1') {
 
+				/**
+				 *
+				 */
+
 				message.ok = true
 				message.data = null
 
+				/**
+				 *
+				 */
+
 				this.___read.buffer = this.___read.buffer.slice(1 + size_as_string.length + 2, this.___read.buffer.length)
+
+				/**
+				 *
+				 */
 
 			} else if (this.___read.buffer.length >= (1 + size_as_string.length + 2 + parseInt(size_as_string) + 2)) {
 
+				/**
+				 *
+				 */
+
 				message.ok = true
 				message.data = this.___read.buffer.slice(index_of_crlf + 2, index_of_crlf + 2 + parseInt(size_as_string)).toString()
+
+				/**
+				 *
+				 */
 
 				this.___read.buffer = this.___read.buffer.slice(1 + size_as_string.length + 2 + parseInt(size_as_string) + 2, this.___read.buffer.length)
 
 			}
 
+			/**
+			 *
+			 */
+
 		} else if (this.___read.buffer[0] === 58) {
+
+			/**
+			 *
+			 */
 
 			message.ok = true
 			message.data = parseInt(this.___read.buffer.slice(1, index_of_crlf).toString())
 
+			/**
+			 *
+			 */
+
 			this.___read.buffer = this.___read.buffer.slice(index_of_crlf + 2, this.___read.buffer.length)
 
+			/**
+			 *
+			 */
+
 		} else if (this.___read.buffer[0] === 45) {
+
+			/**
+			 *
+			 */
 
 			message.ok = true
 			message.error = this.___read.buffer.slice(1, index_of_crlf).toString()
 
+			/**
+			 *
+			 */
+
 			this.___read.buffer = this.___read.buffer.slice(index_of_crlf + 2, this.___read.buffer.length)
 
+			/**
+			 *
+			 */
+
 		} else {
+
+			/**
+			 *
+			 */
 
 			message.ok = true
 			message.error = 'FAILED'
 
 		}
 
+		/**
+		 *
+		 */
+
 		if (!message.ok) {
+
+			/**
+			 *
+			 */
 
 			return
 
 		}
 
+		/**
+		 *
+		 */
+
 		try {
 
+			/**
+			 *
+			 */
+
 			message.data = JSON.parse(message.data)
+
+			/**
+			 *
+			 */
 
 		} catch (failed) {
 
@@ -132,6 +268,10 @@ class Protocol extends events {
 
 		}
 
+		/**
+		 *
+		 */
+
 		this.emit('message', message)
 		this.read()
 
@@ -139,39 +279,99 @@ class Protocol extends events {
 
 	write (args) {
 
+		/**
+		 *
+		 */
+
 		let i = 0
 		let message = ''
 		let stringified = ''
 
+		/**
+		 *
+		 */
+
 		message += `*${args.length}\r\n`
+
+		/**
+		 *
+		 */
 
 		for (i = 0; i < args.length; i++) {
 
+			/**
+			 *
+			 */
+
 			if (!args[i]) {
+
+				/**
+				 *
+				 */
 
 				message += `$0\r\n\r\n`
 
+				/**
+				 *
+				 */
+
 			} else if (args[i].constructor === Number) {
+
+				/**
+				 *
+				 */
 
 				message += `$${Buffer.byteLength((args[i].toString()))}\r\n${args[i]}\r\n`
 
+				/**
+				 *
+				 */
+
 			} else if (args[i].constructor === String) {
+
+				/**
+				 *
+				 */
 
 				message += `$${Buffer.byteLength((args[i]))}\r\n${args[i]}\r\n`
 
+				/**
+				 *
+				 */
+
 			} else if (args[i].constructor === Object) {
+
+				/**
+				 *
+				 */
 
 				stringified = JSON.stringify(args[i])
 
+				/**
+				 *
+				 */
+
 				message += `$${Buffer.byteLength((stringified))}\r\n${stringified}\r\n`
 
+				/**
+				 *
+				 */
+
 			} else {
+
+				/**
+				 *
+				 */
 
 				throw new Error('UNKNOWN_ARGUMENT_TYPE')
 
 			}
 
 		}
+
+		/**
+		 *
+		 */
 
 		return Buffer.from(message)
 
